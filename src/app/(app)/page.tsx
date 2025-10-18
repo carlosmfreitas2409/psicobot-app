1957ab75055b16e42d7ed630958fd3d92f6dfd42
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2025-10-18 10:18:13 -0300

    fix: build

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,11 +1,7 @@
 import { Dashboard } from "@/ui/dashboard";
 
-export default async function DashboardPage({
-  searchParams,
-}: {
-  searchParams: Record<string, string | undefined>;
-}) {
-  const filters = searchParams;
+export default async function DashboardPage({ searchParams }: PageProps<"/">) {
+  const filters = (await searchParams) as Record<string, string | undefined>;
 
   return <Dashboard searchParams={filters} />;
 }
