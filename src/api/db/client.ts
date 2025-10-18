c9ba299c038188b72cef869eb0eaa806b6dadc6e
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2025-10-18 10:06:50 -0300

    chore: add neon database

--- src/api/db/client.ts
+++ src/api/db/client.ts
@@ -1,10 +1,13 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 
 import { env } from "@/env";
 
 import * as schema from "./schema";
 
-export const db = drizzle(env.DATABASE_URL, {
+const sql = neon(env.DATABASE_URL);
+
+export const db = drizzle(sql, {
   casing: "snake_case",
   schema,
 });
